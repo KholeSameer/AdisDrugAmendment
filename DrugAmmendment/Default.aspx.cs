5522c1e205862f9a8104450e9c0f9b094fcac988
Asheesh Kumar <asheesh.kumar@springernature>
2018-01-11 12:29:29 +0530

    added default n unauthorize page

---
+++ DrugAmmendment/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using DrugAmmendment.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DrugAmmendment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private string roles = "admin,editor";
+        private WebAuthorisationProvider _authProvider = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _authProvider = new WebAuthorisationProvider();
+        }
+
+        protected void DrugAmendment_Click(object sender, EventArgs e)
+        {
+            if (SplitString(roles).Any(role => _authProvider.IsInRole(role)))
+            {
+                string siteUrl = System.Configuration.ConfigurationManager.AppSettings["Dashboard"] as string;
+                Response.Redirect(siteUrl);
+            }
+            else
+            {
+                Response.Redirect("UnauthorizePage.aspx");
+            }
+        }
+
+        private static IEnumerable<string> SplitString(string original)
+        {
+            return from piece in original.Split(',')
+                   let trimmed = piece.Trim()
+                   where !String.IsNullOrEmpty(trimmed)
+                   select trimmed;
+        }
+    }
+}
---
+++ DrugAmmendment/UnauthorizePage.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UnauthorizePage.aspx.cs" Inherits="DrugAmmendment.UnauthorizePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 50%; padding-left: 10%;padding-top:10%">
+        
+        <asp:Label Font-Bold="true" Font-Size="20" runat="server"
+            Text="You are not authorize to perform this task.<br />Please contact your admin."></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
